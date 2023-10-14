a82e5661019f71294e2b3435528e8fa1f43a3cd5
naijamesz <kodlikejamesz@hotmail.com>
2023-10-14 19:48:50 +0700

    First commit

---
+++ src/Star.js
@@ -0,0 +1,26 @@
+import React from 'react';
+function Star({ value, color, handleHover, handleHoverLeave, handleClick, isFilled }) {
+  if (!handleHover) {
+    return (
+      <span
+        className='Star'
+        style={{ color }}
+        onMouseEnter={() => console.log('HOVERED IN')}
+        onMouseLeave={() => console.log('HOVERED OUT')}
+        onClick={() => alert('CLICKED A STAR!')}>
+        {isFilled ? '★' : '☆'}
+      </span>
+    );
+  }
+  return (
+    <span
+      className='Star'
+      style={{ color }}
+      onMouseEnter={() => handleHover(value)}
+      onMouseLeave={() => handleHoverLeave(value)}
+      onClick={() => handleClick(value)}>
+      {isFilled ? '★' : '☆'}
+    </span>
+  );
+}
+export default Star;
